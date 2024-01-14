2c6b478608933615110daf2dbc6c93b60d44952a
The Asset Lad <99122894+TheAssetLad@users.noreply.github.com>
2024-01-14 03:30:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,15 @@ from scraper.scrape import scrap_data, get_countries_codes
 # Set Streamlit page configuration
 st.set_page_config(page_title="Inventory Insights", page_icon="📊", layout="wide")
 
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
 # Specify your Google Sheets URL and Worksheet Name or ID
 spreadsheet_url = "https://docs.google.com/spreadsheets/d/1mCVwxzlEY7JtBcSocoecV7JDZYOOW1wk4jE2GgzJtGc/edit#gid=771355186"
 worksheet_name_or_id = "771355186"
 
 # Read data directly from Google Sheets
-data = st.gsheets.read(spreadsheet_url, worksheet_name_or_id)
-
-st.dataframe(data)
-
-# Continue with your other code
-API_KEY = st.secrets.news_api_key["key"]
-API_ENDPOINT = "https://api.newsfilter.io/search?token={}".format(API_KEY)
+data = conn.read(spreadsheet=spreadsheet_url, worksheet=worksheet_name_or_id)
 
 
 # ---------------------------------- Page Styling -------------------------------------
